8a1a0cb14661d9882a6bd55cbeb3f639f2837dba
JYJLK <cho04k@gmail.com>
2025-09-14 04:05:09 +0900

    firebase 연결 완료 로그인 테스트 화면 추가해서 테스트해야함

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
-import 'routes/app_router.dart';
-import 'theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const FocusApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class FocusApp extends StatelessWidget {
-  const FocusApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'stardy log',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: Routes.welcome,
+      title: 'stardylog',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(
+        body: Center(child: Text('Firebase 연결 성공!')),
+      ),
     );
   }
 }
